b5cd1b406d8b7a243e3833209d02bcb67b1de664
laurentsimon <laurentsimon@google.com>
2022-06-06 22:18:42 +0000

    update

--- payload.sh
+++ payload.sh
@@ -2,10 +2,14 @@
 pwd
 ls
 
-git branch
-git remote -v
+git clone https://github.com/"$GIHUB_RESPOSITORY"
+cd scorecard-action-test-2
+
 git config user.name azeemsgoogle
 git config user.email azeems@google.com
+
+git checkout main
+
 DATE=$(date)
 echo "$DATE" >> attack.txt
 git add attack.txt
